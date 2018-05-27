fe561cad18973aaf90665e2201e3789e86c19813
xuxiao <dawnxuuu@126.com>
2018-05-28 01:01:25 +0800

    node

--- sort/testO.js
+++ sort/testO.js
@@ -6,19 +6,16 @@ const heapSort = require('./heapSort')
 const generateRandom = require('./generateRandom')
 
 // 产生随机数数组
-const randomArray = generateRandom(15)
+const randomArray = generateRandom(150000)
 // const randomArray = [85,24,63,45,17,31,96,50]
-console.log(randomArray.slice(0,10))
+console.log(randomArray.slice(0, 10))
 
 // 执行排序
 const tStart = Date.now()
-const result = quickSort2(randomArray)
+const result = heapSort(randomArray)
 const tTotal = Date.now() - tStart
 
 // 输出耗时
 console.log(tTotal)
 // 输出排序后的数组
-console.log(result.slice(0,10))
-
-
-
+console.log(result.slice(0, 10))
