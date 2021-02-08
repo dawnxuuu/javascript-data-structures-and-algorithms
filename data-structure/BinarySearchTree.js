f7b6e62c99cbc851dcbe9827d5a1dce46b0da5a8
徐晓 <dawnxuuu@126.com>
2021-02-08 15:07:07 +0800

    update

--- data-structure/BinarySearchTree.js
+++ data-structure/BinarySearchTree.js
@@ -41,11 +41,12 @@ function BinarySearchTree () {
 
   // 中序遍历
   this.inOrderTraverse = function (callback) {
+    debugger
     var inOrderTraverseNode = function (node, callback) {
       if (node !== null) {
         inOrderTraverseNode(node.left, callback)
         callback(node.key)
-        inOrderTraverseNode(node.right, callback)
+        inOrderTraverseNode(node.right, callback) 
       }
     }
     inOrderTraverseNode(root, callback)
@@ -158,38 +159,16 @@ function BinarySearchTree () {
 }
 
 var tree = new BinarySearchTree()
-// tree.insert(11)
-// tree.insert(7)
-// tree.insert(15)
-// tree.insert(5)
-// tree.insert(3)
-// tree.insert(9)
-// tree.insert(8)
-// tree.insert(10)
-// tree.insert(13)
-// tree.insert(12)
-// tree.insert(14)
-// tree.insert(20)
-// tree.insert(18)
-// tree.insert(25)
-// tree.insert(6)
-
-tree.insert(8)
-tree.insert(3)
-tree.insert(10)
-tree.insert(1)
-tree.insert(6)
-tree.insert(14)
-tree.insert(4)
-tree.insert(7)
-tree.insert(13)
-tree.insert(2)
+var nodes = [8,3,10,1,6,14,4,7,13,2]
+nodes.forEach(element => {
+  tree.insert(element)
+})
 
 function printNode (value) {
   console.log(value)
 }
 
-tree.preOrderTraverse(printNode)
+tree.inOrderTraverse(printNode)
 
 // console.log(tree.search(1) ? '1 found' : '1 not found')
 // console.log(tree.search(8) ? '8 found' : '8 not found')
